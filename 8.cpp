760ebd05db1eabf543ebb55409a277ca669dcae9
atharvachopade5555 <atharvachopade5555@gmail.com>
2025-05-06 07:31:45 +0530

    6ok

--- 8.cpp
+++ 8.cpp
@@ -1,66 +1,90 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-class FlightGraph {
-private:
-vector<vector<int>> adjMatrix;
-vector<string> cities;
-int numCities;
+void con_obst(void);
+void print(int, int);
 
-public:
-FlightGraph(int n) {
-numCities = n;
-adjMatrix.resize(n, vector<int>(n, 0));
-cities.resize(n);
-}
-
-void inputCities() {
-cout << "Enter names of " << numCities << " cities:\n";
-for (int i = 0; i < numCities; i++) {
-cout << "City " << i + 1 << ": ";
-cin >> cities[i];
-}
-}
-
-void addFlightPaths() {
-int time;
-cout << "Enter travel time in hour between cities (enter 0 if no direct flight):\n";
-for (int i = 0; i < numCities; i++) {
-for (int j = 0; j < numCities; j++) {
-if (i != j) {
-cout << "Time from " << cities[i] << " to " << cities[j] << ": ";
-cin >> time;
-adjMatrix[i][j] = time;
-}
-}}
-}
-
-void displayAdjMatrix() {
-cout << "\nAdjacency Matrix Representation:\n";
-cout << "\t";
-for (const auto& city : cities) {
-cout << city << "\t";
-}
-cout << "\n";
-for (int i = 0; i < numCities; i++) {
-cout << cities[i] << "\t";
-for (int j = 0; j < numCities; j++) {
-cout << adjMatrix[i][j] << "\t";
-}
-cout << endl;
-}
-}
-};
+float a[20], b[20], wt[20][20], c[20][20];
+int r[20][20], n;
 
 int main() {
-int n;
-cout << "Enter number of cities: ";
-cin >> n;
-FlightGraph graph(n);
-graph.inputCities();
-graph.addFlightPaths();
-graph.displayAdjMatrix();
-return 0;
+    int i;
+    cout << "\n**** PROGRAM FOR OBST ******\n";
+    cout << "\nEnter the number of nodes: ";
+    cin >> n;
+
+    cout << "\nEnter the probability for successful search :: ";
+    cout << "\n---------------------------------------------\n";
+    for (i = 1; i <= n; i++) {
+        cout << "p[" << i << "]: ";
+        cin >> a[i];
+    }
+
+    cout << "\nEnter the probability for unsuccessful search :: ";
+    cout << "\n-----------------------------------------------\n";
+    for (i = 0; i <= n; i++) {
+        cout << "q[" << i << "]: ";
+        cin >> b[i];
+    }
+
+    con_obst();
+    print(0, n);
+    cout << endl;
+
+    return 0;
+}
+
+void con_obst(void) {
+    int i, j, l;
+
+    for (i = 0; i < n; i++) {
+        c[i][i] = 0.0;
+        r[i][i] = 0;
+        wt[i][i] = b[i];
+
+        wt[i][i + 1] = b[i] + b[i + 1] + a[i + 1];
+        c[i][i + 1] = wt[i][i + 1];
+        r[i][i + 1] = i + 1;
+    }
+
+    c[n][n] = 0.0;
+    r[n][n] = 0;
+    wt[n][n] = b[n];
+
+    for (i = 2; i <= n; i++) {
+        for (j = 0; j <= n - i; j++) {
+            wt[j][j + i] = wt[j][j + i - 1] + a[j + i] + b[j + i];
+            c[j][j + i] = 9999;
+
+            for (l = j + 1; l <= j + i; l++) {
+                if (c[j][j + i] > (c[j][l - 1] + c[l][j + i])) {
+                    c[j][j + i] = c[j][l - 1] + c[l][j + i];
+                    r[j][j + i] = l;
+                }
+            }
+
+            c[j][j + i] += wt[j][j + i];
+        }
+        cout << endl;
+    }
+
+    cout << "\n\nOptimal BST is :: ";
+    cout << "\nw[0][" << n << "] :: " << wt[0][n];
+    cout << "\nc[0][" << n << "] :: " << c[0][n];
+    cout << "\nr[0][" << n << "] :: " << r[0][n];
+}
+
+void print(int l1, int r1) {
+    if (l1 >= r1)
+        return;
+
+    if (r[l1][r[l1][r1] - 1] != 0)
+        cout << "\nLeft child of " << r[l1][r1] << " :: " << r[l1][r[l1][r1] - 1];
+
+    if (r[r[l1][r1]][r1] != 0)
+        cout << "\nRight child of " << r[l1][r1] << " :: " << r[r[l1][r1]][r1];
+
+    print(l1, r[l1][r1] - 1);
+    print(r[l1][r1], r1);
 }
 
